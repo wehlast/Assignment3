e6a027fbb4cc96f698c623ce550aefb4ccc47ef9
Mads Wehlast Hansen <wehlast@gmail.com>
2017-02-23 18:30:09 +0100

    Made fade in and out function and switcher function and made pictures avalible for testing

--- app/src/main/java/assignment3/assignment3/MainActivity.java
+++ app/src/main/java/assignment3/assignment3/MainActivity.java
@@ -2,6 +2,8 @@ package assignment3.assignment3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Let the ViewSwitcher do the animation listening for you
+        ((ViewSwitcher) findViewById(R.id.switcher)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ViewSwitcher switcher = (ViewSwitcher) v;
+
+                if (switcher.getDisplayedChild() == 0) {
+                    switcher.showNext();
+                } else {
+                    switcher.showPrevious();
+                }
+            }
+        });
     }
 }
